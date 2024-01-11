32b35eb90fce9751859bd82ab5b1a496d5fbf8b2
YuJungHyun <0703olivia@naver.com>
2024-01-11 21:15:19 +0900

    [Chore] Fragment name 변경

--- feature/assignment/src/main/java/com/one/feature/assignment/teacher/AssignmentTFragment.kt
+++ feature/assignment/src/main/java/com/one/feature/assignment/teacher/AssignmentTFragment.kt
@@ -14,10 +14,10 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [AssginmentTFragment.newInstance] factory method to
+ * Use the [AssignmentTFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class AssginmentTFragment : Fragment() {
+class AssignmentTFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -50,7 +50,7 @@ class AssginmentTFragment : Fragment() {
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            AssginmentTFragment().apply {
+            AssignmentTFragment().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
