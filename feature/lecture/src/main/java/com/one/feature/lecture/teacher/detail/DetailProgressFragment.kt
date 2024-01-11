2a6ed0bb49ab96cfd172206130afc2a9dc405e61
jinee <sksowk156@gmail.com>
2024-01-11 21:28:41 +0900

    [Fix] package name 에러, menu inflate 에러

--- feature/lecture/src/main/java/com/one/feature/lecture/teacher/detail/DetailProgressFragment.kt
+++ feature/lecture/src/main/java/com/one/feature/lecture/teacher/detail/DetailProgressFragment.kt
@@ -1,11 +1,11 @@
 package com.one.feature.lecture.teacher.detail
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.one.feature.assignment.R
+import androidx.fragment.app.Fragment
+import com.one.feature.lecture.R
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
