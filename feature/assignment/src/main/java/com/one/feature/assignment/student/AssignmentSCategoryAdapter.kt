619d567402e579584c2315c38515dd9627cc7ad9
OliviaYJH <0703olivia@naver.com>
2024-02-04 12:49:30 +0900

    [Feat] 학생/학부모 숙제 tab animation 구현

---
+++ feature/assignment/src/main/java/com/one/feature/assignment/student/AssignmentSCategoryAdapter.kt
@@ -0,0 +1,81 @@
+package com.one.feature.assignment.student
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.LinearLayout
+import androidx.core.view.isVisible
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.one.feature.assignment.databinding.ItemAssignmentSRecyclerBinding
+
+class AssignmentSCategoryAdapter: RecyclerView.Adapter<AssignmentSCategoryAdapter.AssignmentSCategoryViewHolder>() {
+    var assignmentSCategoryList = arrayListOf<AssignmentCategoryData>()
+
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): AssignmentSCategoryViewHolder {
+        val binding = ItemAssignmentSRecyclerBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return AssignmentSCategoryViewHolder(binding)
+    }
+
+    override fun getItemCount(): Int = assignmentSCategoryList.size
+    override fun onBindViewHolder(holder: AssignmentSCategoryViewHolder, position: Int) {
+        val data = assignmentSCategoryList[position]
+        holder.bind(data)
+    }
+
+    inner class AssignmentSCategoryViewHolder(val binding: ItemAssignmentSRecyclerBinding): RecyclerView.ViewHolder(binding.root) {
+        fun bind(data: AssignmentCategoryData) {
+            binding.tvCategoryAssignmentS.text = data.category
+            binding.tvTeacherNameAssignmentS.text = data.teacherName
+
+            val layoutExpand = binding.linearlayoutAssignmentListAssignmentS
+                binding.btnToggleDown.setOnClickListener {
+                val show = toggleLayout(
+                    !layoutExpand.isVisible,
+                    it.findViewById(binding.btnToggleDown.id),
+                    layoutExpand
+                )
+                    layoutExpand.isVisible = show
+                    getAssignmentList()
+            }
+        }
+
+        private fun getAssignmentList() {
+            with(binding) {
+                val assignmentListAdapter = AssignmentSAssignmentListAdapter()
+
+                val assignmentList: ArrayList<AssignmentListData> = ArrayList()
+                assignmentList.add(AssignmentListData("first"))
+                assignmentList.add(AssignmentListData("second"))
+                assignmentList.add(AssignmentListData("third"))
+
+                rvAssignmentListAssignmentS.adapter = assignmentListAdapter
+                rvAssignmentListAssignmentS.layoutManager = LinearLayoutManager(itemView.context)
+                assignmentListAdapter.assignmentList = assignmentList
+                assignmentListAdapter.notifyDataSetChanged()
+            }
+        }
+
+        private fun toggleLayout(
+            isExpanded: Boolean,
+            view: View,
+            layoutExpand: LinearLayout
+        ): Boolean {
+            AssignmentSAnimation.toggleArrow(view, isExpanded)
+            if (isExpanded) AssignmentSAnimation.expandView(layoutExpand)
+            else AssignmentSAnimation.collapseView(layoutExpand)
+
+            return isExpanded
+        }
+    }
+
+}
+
+data class AssignmentCategoryData(
+    val category: String, // 과목
+    val teacherName: String
+)
+
--- feature/assignment/src/main/java/com/one/feature/assignment/student/AssignmentSFragment.kt
+++ feature/assignment/src/main/java/com/one/feature/assignment/student/AssignmentSFragment.kt
@@ -1,5 +1,6 @@
 package com.one.feature.assignment.student
 
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.one.core.common_ui.base.BaseFragment
 import com.one.feature.assignment.databinding.FragmentAssginmentSBinding
 
@@ -7,11 +8,28 @@ import com.one.feature.assignment.databinding.FragmentAssginmentSBinding
 class AssignmentSFragment : BaseFragment<FragmentAssginmentSBinding>(FragmentAssginmentSBinding::inflate) {
     override fun onViewCreated() {
         initToolbar()
+        initRecyclerView()
     }
 
     private fun initToolbar(){
         binding.toolbarAssignments.initBackKey()
         binding.toolbarAssignments.initTitle("숙제")
+    }
+
+    private fun initRecyclerView() {
+        with(binding) {
+            val assignmentAdapter = AssignmentSCategoryAdapter()
+
+            val categoryList: ArrayList<AssignmentCategoryData> = ArrayList()
+            categoryList.add(AssignmentCategoryData("국어", "김민아"))
+            categoryList.add(AssignmentCategoryData("수학", "박수민"))
+            categoryList.add(AssignmentCategoryData("영어", "김민아"))
+
+            rvAssignmentS.adapter = assignmentAdapter
+            rvAssignmentS.layoutManager = LinearLayoutManager(context)
+            assignmentAdapter.assignmentSCategoryList = categoryList
+            assignmentAdapter.notifyDataSetChanged()
+        }
     }
 
 }
